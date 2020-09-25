ae9ec9a57df1be62835e57fe62f078e5cbf451ba
Andrea Crotti <anc@juxt.pro>
2020-09-25 15:53:13 +0100

    add couple of extra packages

--- setup.sh
+++ setup.sh
@@ -2,15 +2,16 @@
 
 NETWORK_PACKAGES="dhcpcd netctl dialog wpa_supplicant openssh networkmanager network-manager-applet"
 XORG="xorg-server xorg-xinit xorg-apps mesa-libgl xf86-input-libinput termite"
-DEV_PACKAGES="man-db man-pages vim emacs tmux ack git the_silver_searcher asciidoctor npm"
+DEV_PACKAGES="man-db man-pages vim emacs tmux ack git the_silver_searcher asciidoctor npm git-lfs"
 LAPTOP_SPECIFIC="intel-ucode brightnessctl bluez bluez-utils blueman"
 WEB_DEV="firefox chromium lightdm lightdm-gtk-greeter"
 FONTS="ttf-liberation ttf-dejavu"
 WM="i3 dmenu"
 JAVA="jdk11-openjdk visualvm clojure rlwrap leiningen"
 AUDIO="pulseaudio pavucontrol"
+SECURITY="gnupg keybase"
 
 YAY_PACKAGES="teams"
 
-sudo pacman --needed -S $NETWORK_PACKAGES $XORG $DEV_PACKAGES $LAPTOP_SPECIFIC $WEB_DEV $FONTS $WM $JAVA $AUDIO
+sudo pacman --needed -S $NETWORK_PACKAGES $XORG $DEV_PACKAGES $LAPTOP_SPECIFIC $WEB_DEV $FONTS $WM $JAVA $AUDIO $SECURITY
 yay -S --noconfirm $YAY_PACKAGES
